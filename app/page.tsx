9c0bea3854c88e5f08a81b75155ee4ce1dc4c2db
sembauke <semboot699@gmail.com>
2024-03-29 14:13:13 +0100

    feat: first commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,93 +3,116 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.leaderboard}>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <Image alt="Mountains" src="/logo.webp" layout="fill" />
         </div>
+        <h1 className={styles.title}>Q1</h1>
+
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <table>
+        <tr>
+          <td>1</td>
+          <td>LEC</td>
+          <td>1:45.337</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>PIA</td>
+          <td>+1.363</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>ALB</td>
+          <td>+1.394</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>PER</td>
+          <td>+2.276</td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>GAS</td>
+          <td>+2.278</td>
+        </tr>
+        <tr>
+          <td>6</td>
+          <td>NOR</td>
+          <td>+2.629</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>SAR</td>
+          <td>+2.735</td>
+        </tr>
+        <tr>
+          <td>8</td>
+          <td>TSU</td>
+          <td>+2.798</td>
+        </tr>
+        <tr>
+          <td>9</td>
+          <td>DEV</td>
+          <td>+3.032</td>
+        </tr>
+        <tr>
+          <td>10</td>
+          <td>BOT</td>
+          <td>+3.161</td>
+        </tr>
+        <tr>
+          <td>11</td>
+          <td>SAI</td>
+          <td>+3.303</td>
+        </tr>
+        <tr>
+          <td>12</td>
+          <td>HAM</td>
+          <td>+3.501</td>
+        </tr>
+        <tr>
+          <td>13</td>
+          <td>MAG</td>
+          <td>+3.519</td>
+        </tr>
+        <tr>
+          <td>14</td>
+          <td>SAR</td>
+          <td>+3.558</td>
+        </tr>
+        <tr>
+          <td>15</td>
+          <td>HUL</td>
+          <td>+3.814</td>
+        </tr>
+        <tr>
+          <td>16</td>
+          <td>VER</td>
+          <td>+4.037</td>
+        </tr>
+        <tr>
+          <td>17</td>
+          <td>GUA</td>
+          <td>+4.424</td>
+        </tr>
+        <tr>
+          <td>18</td>
+          <td>OCO</td>
+          <td>+4.429</td>
+        </tr>
+        <tr>
+          <td>19</td>
+          <td>ALO</td>
+          <td>+4.998</td>
+        </tr>
+        <tr>
+          <td>20</td>
+          <td>RUS</td>
+          <td>+5.591</td>
+        </tr>
+      </table>
+    </div>
   );
 }
